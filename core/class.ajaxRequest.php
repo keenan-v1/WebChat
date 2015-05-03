5abd3ddee0b7731eee74cbfc4effad793198b83e
Jonathan Walker <xorith@gmail.com>
2015-05-03 01:09:32 -0400

    Dev Update

--- core/class.ajaxRequest.php
+++ core/class.ajaxRequest.php
@@ -1,6 +1,12 @@
 <?php
 // class.ajaxRequest.php
 
+function logWrite($msg) {
+   $fp = fopen("./log.txt", 'a');
+   fwrite($fp, $msg."\n");
+   fclose($fp);
+}
+
 /*
  * AjaxRequest: A class handling specific AJAX requests.
  */
@@ -13,40 +19,62 @@ class AjaxRequest {
     );
     
     // Reply Types
-    const TYPE_JSON = "JSON";
-    const TYPE_PLAIN = "text/plain";
+    const JSON = "application/json";
+    const PLAIN = "text/plain";
     
     // Request Types
-    const TYPE_GET = "GET";
-    const TYPE_POST = "POST";
-    const TYPE_OPTIONS = "OPTIONS";
+    const GET = "GET";
+    const POST = "POST";
+    const OPTIONS = "OPTIONS";
     
     // Reply Data
     private $replyData = array();
-    private $replyType = self::TYPE_JSON;
+    private $replyType = self::JSON;
     
     // Request Data
     private $requestData = array();
     private $serverData = array();
-    private $requestType = self::TYPE_POST;
+    private $requestType = self::JSON;
+    private $requestMethod = self::POST;
     private $requestAction = "";
+    private $output = array();
     
-    public function __construct($requestType, $requestData, $serverData = null) {
-        switch($requestType) {
-            case self::TYPE_GET:
-                $this->requestType = self::TYPE_GET;
+    public function __construct($requestData = null) {
+        $raw = file_get_contents("php://input");
+        logWrite("Raw: ".print_r($raw,true));
+	logWrite("Server: ".print_r($_SERVER,true));
+        $this->serverData = $_SERVER;
+
+        // What is our content type?
+       	$cType = explode(", ", $_SERVER['HTTP_ACCEPT']);
+	switch($cType[0]) {
+		case self::JSON:
+			$this->requestType = self::JSON;
+			$this->requestData = json_decode($raw, true);
+			break;
+		default:
+			$this->requestType = self::PLAIN;
+			$this->requestData = $raw;
+			break;
+	}
+
+        logWrite("cType: ".print_r($cType,true));
+
+        switch($_SERVER['REQUEST_METHOD']) {
+            case self::GET:
+                $this->requestMethod = self::GET;
                 break;
-            case self::TYPE_POST:
-                $this->requestType = self::TYPE_POST;
+            case self::POST:
+                $this->requestMethod = self::POST;
                 break;
-            case self::TYPE_OPTIONS:
-                $this->requestType = self::TYPE_OPTIONS;
+            case self::OPTIONS:
+                $this->requestMethod = self::OPTIONS;
                 $this->write();
                 return;
             default:
-                throw new Exception("Invalid Request Type: {$requestType}");
+                throw new Exception("Invalid Request Type");
         }
-        $this->requestData = $requestData;
+        
         if(is_array($serverData) && !empty($serverData))
             $this->serverData = $serverData;
         else if(is_array(($serverData = $_SERVER)) && !empty($serverData))
@@ -54,24 +82,17 @@ class AjaxRequest {
         else
             throw new Exception("No Server Data or invalid data: ", $serverData);
 
-        if(!isset($this->requestData['action']) || empty($this->requestData['action']))
-            throw new Exception("No action in request.");
-        else
-            $this->requestAction = $this->requestData['action'];
-
-        if(!array_key_exists($this->requestAction, self::$actions))
-            throw new Exception("Unsupported Action: ".$this->requestAction);
+	foreach($this->requestData as $req){
+		if(!isset($req['action']) || empty($req['action']))
+	            throw new Exception("No action in request.");
+		if(!array_key_exists($req['action'], self::$actions))
+	            throw new Exception("Unsupported Action: ".$this->requestAction);
+                $action = $req['action'];
+		$handler = self::$actions[$action];
+		$this->$handler($req['data']);
+	}
     }
     
-    public function processRequest() {
-        if($this->requestType != self::TYPE_OPTIONS) {
-            $handler = self::$actions[$this->requestAction];
-            // Handle it!
-            $this->$handler();
-        }
-        // Write output back to requester
-        $this->write();
-    }
        
     // Sets the "keyword" (arg1) with the "value" (arg2)
     // If arg1 is an array of key-value pairs, we just append it.
@@ -86,32 +107,43 @@ class AjaxRequest {
     
     // Encode our data as JSON and send it.
     public function writeJson() {
+	header('Content-type: '.self::JSON);
         $replyJson = json_encode($this->replyData);
-        echo $replyJson;
+        $this->output[]= $replyJson;
         return;        
     }
     
     // Wrapper for different data types (future proofing)
     public function write() {
         switch($this->replyType) {
-            case self::TYPE_JSON:
+            case self::JSON:
+                logWrite(print_r($this, true));
                 $this->writeJson();
                 break;
             default:
                 throw new Exception("Unsupported data type: ".$this->replyType);
         }
+		$this->replyData = array();
     }
     
+	public function flush() {
+	echo json_encode($this->output);
+}
     // Handlers
     
-    public function test_function() {
+    public function test_function($data) {
         $this->setData("test-result", "passed");
-        $this->setData("requestType", $this->requestType);
-        $this->setData("requestData", json_encode($this->requestData));
+	foreach($data as $key => $value)
+		$this->setData("request-{$key}", $value);
+        $this->setData("requestData", json_encode($data));
+	$this->setData("status","success");
+	$this->write();
     }
     
-    public function echo_function() {
-        $this->setData($this->requestData);
+    public function echo_function($data) {
+        $this->setData($data);
+	$this->setData("status","success");
+	$this->write();
     }
 }
 
---
+++ core/test.php
@@ -0,0 +1,8 @@
+<?php
+
+$var1 = "Hello";
+$var2 = "World";
+
+echo $var1 . " " . $var2 . "\n";
+
+?>
